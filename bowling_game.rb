dbd83f6384d2c54ec6c944aabb9c67fe4894f57b
MakerSquare Student <students@themakersquare.com>
2013-11-21 16:33:23 -0600

    Created strike score for the Bowling game

--- bowling_game.rb
+++ bowling_game.rb
@@ -24,16 +24,18 @@ class BowlingGame
       roll      = @rolls[current_roll]
       next_roll = @rolls[current_roll + 1]
 
-      if roll + next_roll == 10
+      if roll == 10
+        total_score += 10 + next_roll + @rolls[current_roll + 2]
+        current_roll += 1
+      elsif roll + next_roll == 10
         total_score += 10 + @rolls[current_roll + 2]
+        current_roll += 2
       else
         total_score += roll + next_roll
+        current_roll += 2
       end
-
-      current_roll += 2
     end
 
     return total_score
   end
-
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -34,5 +34,15 @@ describe BowlingGame do
 		    expect(game.score).to eq(20)
 		  end
 		end
+		context 'when a strike is thrown' do
+			it 'records the frame score as 10 plus the total number of pins knocked down in the next rolls' do
+				game.roll(10)
+			  game.roll(7)
+			  game.roll(1)
+		    16.times { game.roll(0) }
+
+			  expect(game.score).to eq(26)
+		  end
+		end
 	end
 end
