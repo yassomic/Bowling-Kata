3ed355792cc69e423f787abda37e8b11b3039e9f
MakerSquare Student <students@themakersquare.com>
2013-11-21 15:50:27 -0600

    Updated test with let expression

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -4,25 +4,20 @@ require 'rspec'
 require_relative '../bowling_game'
 
 describe BowlingGame do
+	let(:game) {BowlingGame.new}
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
-      game = BowlingGame.new
-
       expect(game.rolls).to eq([])
     end
   end
 
   describe '#score' do
 	  it 'scores the game as 0 when only gutter balls are thrown' do
-	    game = BowlingGame.new
 	    20.times { game.roll(0) }
-
 	    expect(game.score).to eq(0)
 	  end
 	  it 'scores the game as 20 when one pin is knocked down on each roll' do
-  		game = BowlingGame.new
   		20.times { game.roll(1) }
-
   		expect(game.score).to eq(20)
 		end
 	end
