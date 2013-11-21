95000c87d90f66cbdb508f9d37f9c167785a4712
MakerSquare Student <students@themakersquare.com>
2013-11-21 15:30:57 -0600

    Add passing roll method and score method in BowlingGame class.

--- bowling_game.rb
+++ bowling_game.rb
@@ -4,4 +4,16 @@ class BowlingGame
   def initialize
     @rolls = []
   end
+
+  def roll(pins)
+ 		@pins = pins
+  end
+
+# Returns the Integer score for this game. Expects to be run after all rolls
+# for the game have been recorded.
+  def score
+  	@pins
+  end
+
+
 end
