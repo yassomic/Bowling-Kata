f22f6734db8dc2725985833745bb02b638b8a130
MakerSquare Student <students@themakersquare.com>
2013-11-21 15:43:12 -0600

    Created total score for bowling game.

--- bowling_game.rb
+++ bowling_game.rb
@@ -5,14 +5,19 @@ class BowlingGame
     @rolls = []
   end
 
+# Record a roll in the game.
+#
+# pins - The Integer number of pins knocked down in this roll.
+#
+# Returns nothing.
   def roll(pins)
- 		@pins = pins
+ 		@rolls << pins
   end
 
 # Returns the Integer score for this game. Expects to be run after all rolls
 # for the game have been recorded.
   def score
-  	@pins
+  	@rolls.reduce(:+)
   end
 
 
